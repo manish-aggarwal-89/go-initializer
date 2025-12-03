0b930d2375008d39d557986d2756e9883f1b38d0
manish-aggarwal-89 <manish.aggarwal@tiket.com>
2025-12-03 18:48:29 +0530

    fix go mod

--- service-template/cli/go.mod
+++ service-template/cli/go.mod
@@ -1,4 +1,4 @@
-module github.com/manish-aggarwal/go-initializer/service-template/cli
+module github.com/manish-aggarwal-89/go-initializer/service-template/cli
 
 go 1.22
 
--- service-template/cli/main.go
+++ service-template/cli/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/manish-aggarwal/go-initializer/service-template/cli/cmd"
+import "github.com/manish-aggarwal-89/go-initializer/service-template/cli/cmd"
 
 func main() {
 	cmd.Execute()
